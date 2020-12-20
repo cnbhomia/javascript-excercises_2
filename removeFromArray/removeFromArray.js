931c0c02bb8496998f1e06a41d51103513904c97
cnbhomia <cnbhomia@gmail.com>
2020-12-20 00:25:23 -0500

    finishing 1st level of excercises

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,39 @@
-const removeFromArray = function() {
+//a function defined with no arguments can accept an argument
+//accessible using keyword `arguments`
+const removeFromArray = function() { 
+
+    //converting arguments into an array
+    let args=[];
+    for (let i=0; i<arguments.length; i++){
+        args[i]=arguments[i]
+    }
+    // alternative method to this function is
+    //let args = Array.prototype.slice.call(arguments)
+
+    const parent_arr = args[0];
+    args.shift()
+    console.log(parent_arr);
+    console.log(args);
+
+    //loop over parent and args to compare
+
+    let new_arr =[];
+
+    for(let k=0; k<parent_arr.length; k++){
+        let counter =0
+        for(let j= 0; j<args.length; j++){
+        
+            if(parent_arr[k] === args[j]){
+                counter += 1;
+                               
+            }
+        }
+        if(counter == 0){
+            new_arr.push(parent_arr[k])
+        }
+    }
+
+    return new_arr ;
 
 }
 
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,41 @@
+const validation = function(args){
+    for(let i=0; i<args.length; i++){
+        if((typeof args[i] != "number") || args[i]<0 ){
+            console.log("ERROR",args)
+            return true;
+        }
+    }
+}
+
+const sumIt = function(args){
+    let sum =0;
+    
+    if(args[0] < args[args.length-1]){
+    //loop through the values of args and 
+        for(let i=args[0]; i<= args[args.length-1]; i++){ 
+            sum += i;
+            
+        }
+    }else{
+        for(let i=args[args.length-1]; i <=args[0]; i++){ 
+            sum += i;
+        }
+       
+    }
+    console.log('sum is:',sum)
+    return sum;
+
+}
+
 const sumAll = function() {
+    let args = [...arguments]
+    console.log(args)
+    
+    //if validation is false, return ERROR else return sum
+    return (validation(args) == true)?'ERROR':sumIt(args)
+ 
+    
+
 
 }
 
